fa154e7e43e72bf4669ba7636f77f85f2403058c
Nyansuke01 <51296886+nyannsuke01@users.noreply.github.com>
2019-09-20 13:59:38 +0900

    Update.view.php
    テキスト修正

--- cafe talk/view.php
+++ cafe talk/view.php
@@ -16,7 +16,7 @@ $posts->execute(array($_REQUEST['id']));
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>ひとこと掲示板</title>
+	<title>カフェ掲示板</title>
 
 	<link rel="stylesheet" href="style.css" />
 </head>
@@ -24,7 +24,7 @@ $posts->execute(array($_REQUEST['id']));
 <body>
 <div id="wrap">
   <div id="head">
-    <h1>ひとこと掲示板</h1>
+    <h1>カフェ掲示板</h1>
   </div>
   <div id="content">
   <p>&laquo;<a href="index.php">一覧にもどる</a></p>
